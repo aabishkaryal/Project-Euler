65cbe12325af15255f4f8c7ea2d8ec97bc8938e4
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-03-16 22:45:00 +0545

    Added main execution for problem 1 and 2

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	problem1 "github.com/aabishkaryal/Project-Euler/Problem1"
+	problem2 "github.com/aabishkaryal/Project-Euler/Problem2"
+)
 
 func main() {
 	fmt.Println("Hello Project Euler")
+	fmt.Println("Problem 1: ", problem1.Solution(1000))
+	fmt.Println("Problem 2: ", problem2.Solution(4_000_000))
 }
